3daf5e499d1c5fb65d7d0a801da6678f80772586
samuer216progra <srodriguez-2022165@kinal.edu.gt>
2024-02-24 20:19:53 -0600

    se levanto el servidor

--- db/config.js
+++ db/config.js
@@ -1,14 +1,13 @@
 const mongoose = require('mongoose');
 
 const dbConnection = async () => {
+    try {
+        await mongoose.connect(process.env.MONGODB_CNN, {});
+        
+        console.log('Base de datos conectada');
+    } catch (e) {
 
-    try{
-        await mongoose.connect(process.env.MONGODB_CNN,{});
-        console.log('Base de datos Conectada');
-
-
-    }catch(e){
-        throw new Error('No se Conecto',e)
+        throw new Error('Error al conectar la base de datos', e);
     }
 }
 
--- models/server.js
+++ models/server.js
@@ -1,21 +1,38 @@
-const express = rquire('express');
+const express = require('express');
 const cors = require('cors');
-const {dbConnection} = require('');
+const { dbConnection } = require('../db/config');
 
 class Server{
-    constructor(){
-        this.app = express();
-        this.port = process .env.PORT;
-        this.usuarioPath = '/api/estuadiantes';
 
-        this.conectarDB
+    constructor(){
+
+        this.app = express();
+        this.port = process.env.PORT;
+
+
+        this.conectarDB();
+        this.middleware();
+       
+    }
+
+    async conectarDB(){
+        await dbConnection();
+    }
+
+    middleware(){
+
+        this.app.use(express.static('public'));
+        this.app.use(cors());
+        this.app.use(express.json());
     }
 
     listen(){
+        
         this.app.listen(this.port, () =>{
-        console.log('Servidor Ejecutandose',this.port)
-    });
+            console.log('Servidor ejecutandose y escuchando el puerto', this.port)
+        });
     }
+    
 }
 
 module.exports = Server;
